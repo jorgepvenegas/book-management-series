6758bfaf71d0b9514d147b0b0444adc55c87cc81
Jorge Venegas <jorgepvenegas@protonmail.com>
2025-07-17 23:29:43 -0700

    video 1

---
+++ packages/api/src/db/schema.ts
@@ -0,0 +1,7 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const booksTable = pgTable("books", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+  year: integer().notNull(),
+});
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,10 +1,28 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { booksTable } from "./db/schema.js";
 
 const app = new Hono();
 
+const db = drizzle(process.env.DATABASE_URL!);
+
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+app.post("/book", async (c) => {
+  const body = await c.req.json();
+
+  const newBook: typeof booksTable.$inferInsert = {
+    title: body.title,
+    year: body.year,
+  };
+
+  const result = await db.insert(booksTable).values(newBook).returning();
+
+  return c.json(result, { status: 200 });
 });
 
 serve(
