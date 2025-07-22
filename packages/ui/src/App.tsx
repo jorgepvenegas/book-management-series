29c0e23e990ae172a498631052a35fe55aa476b2
Jorge Venegas <jorgepvenegas@protonmail.com>
2025-07-21 22:36:54 -0700

    add dialog, table, dropdown-menu, modal

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,10 +1,85 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { Button } from "./components/ui/button";
+import { EllipsisIcon } from "lucide-react";
+import { useState } from "react";
+import Modal from "./components/modal";
+
+const testData = [
+  {
+    id: 1,
+    title: "Love in the Time of Cholera",
+    author: "Gabriel García Márquez",
+  },
+  {
+    id: 2,
+    title: "Gulliver’s Travels",
+    author: "Jonathan Swift",
+  },
+  {
+    id: 3,
+    title: "To the Lighthouse",
+    author: "Virginia Woolf",
+  },
+];
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <div className="w-full flex justify-center h-screen bg-gray-100">
+      <Modal open={modalOpen} onOpenChange={setModalOpen} />
+      <div className="w-3/4 p-8 bg-white">
+        <div className="flex justify-between pb-8">
+          <h1 className="text-2xl">Books</h1>
+          <Button onClick={() => setModalOpen(true)}>Add Book</Button>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Id</TableHead>
+              <TableHead>Title</TableHead>
+              <TableHead>Author</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {testData.map((book) => (
+              <TableRow>
+                <TableCell className="font-medium">{book.id}</TableCell>
+                <TableCell>{book.title}</TableCell>
+                <TableCell>{book.author}</TableCell>
+                <TableCell>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <EllipsisIcon />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      <DropdownMenuItem onClick={() => setModalOpen(true)}>
+                        Edit
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>Delete</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
   );
 }
 
---
+++ packages/ui/src/components/modal.tsx
@@ -0,0 +1,31 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+function Modal({
+  open,
+  onOpenChange,
+}: {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}) {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your account and
+            remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default Modal;
